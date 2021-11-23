1b482658952260b0d86497e013b62377e0c3c3f2
Raúl Marín <rmarin82@gmail.com>
2021-11-23 12:15:27 +0100

    Install and configure babel-plugin-import and next-plugin-antd-less

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withAntdLess = require("next-plugin-antd-less");
+
+module.exports = withAntdLess({
+  // optional: you can modify antd less variables directly here
+  modifyVars: { "@primary-color": "green" },
+  // Or better still you can specify a path to a file
+  lessVarsFilePath: "./styles/variables.less",
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {},
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+});
